974cecc37aac1a564d317dc7091f445b9a32dfac
Indragie Karunaratne <i@indragie.com>
2025-06-25 21:35:32 -0700

    Import blog posts

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -18,11 +18,11 @@ export function BlogPosts() {
         .map((post) => (
           <Link
             key={post.slug}
-            className="flex flex-col space-y-1 mb-4"
+            className="block mb-4 no-underline"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
+            <div className="grid grid-cols-1 md:grid-cols-[140px_1fr] gap-2 items-baseline">
+              <p className="text-neutral-600 dark:text-neutral-400 tabular-nums">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
